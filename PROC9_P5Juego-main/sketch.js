746a5c055d1defe60bf3a001dd6646ded819553a
3LADRIAN <93840977+3LADRIAN@users.noreply.github.com>
2021-11-06 12:55:49 -0700

    Add files via upload
    primera prueba para subir código

---
+++ PROC9_P5Juego-main/sketch.js
@@ -0,0 +1,33 @@
+var caja;
+
+function setup() {
+  createCanvas(400,400);
+  caja=createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(35);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    caja.position.x = caja.position.x +5;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    caja.position.x = caja.position.x -5;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    caja.position.y = caja.position.y -5;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    caja.position.y = caja.position.y +5;
+  }
+
+  drawSprites();
+}
+
+
+
+
